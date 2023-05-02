0c44b083d65e92cc1a7b6d043a10ae8fe43d5db6
xyoouy <egorloparev@gmail.com>
2023-05-02 17:16:01 +0500

    Add user authorization

---
+++ db.js
@@ -0,0 +1,11 @@
+// const Pool = require('pg').Pool;
+// const pool = new Pool({
+//     user: "postgres",
+//     password: "mypassword",
+//     host: "localhost",
+//     port: 5432,
+//     database: "registration"
+// });
+//
+//
+// module.exports = pool;
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const app = express();
+const userRouter = require('./routes/user.routes');
 
+const app = express();
 const PORT = 8080;
 const HOST = 'localhost';
-app.use(express.static('public'));
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
+
+app.use(express.json());
+app.use('/api', userRouter);
 
 app.listen(PORT, () => {
     console.log(`Server started: http://${HOST}:${PORT}`);
