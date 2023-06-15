a9c72d80ac65a41eb269c430e70d173d2b9db393
xyoouy <egorloparev@gmail.com>
2023-06-15 14:46:58 +0500

    Fix commit

---
+++ Server/Application/Questions/Commands/AddQuestion/AddQuestionCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Application.Questions.Commands.AddQuestion;
+
+public class AddQuestionCommand : IRequest<int>
+{
+    public int TypeId { get; set; }
+    public int QuizId { get; set; }
+    public string Content { get; set; }
+}
---
+++ Server/Web/Controllers/QuestionController.cs
@@ -0,0 +1,15 @@
+﻿using Application.Questions.Commands.AddQuestion;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers;
+
+[Route("api/[controller]")]
+public class QuestionController : BaseController
+{
+    [HttpPost("add")]
+    public async Task<ActionResult<int>> AddQuestion([FromBody] AddQuestionCommand command)
+    {
+        var questionId = await Mediator.Send(command);
+        return Ok(questionId);
+    }
+}
