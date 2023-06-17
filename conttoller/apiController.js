3b41ba2ee0c01ce571db4b70a6487f8c385025d4
bolthen <92172378+bolthen@users.noreply.github.com>
2023-06-17 17:59:19 +0500

    build active quizzes page by api response

--- conttoller/apiController.js
+++ conttoller/apiController.js
@@ -16,30 +16,82 @@ class ApiController {
     }
 
     async getQuizzes(userData) {
-        const data = {
+        const data =
+        {
             "groupVm1": {
-                "id": '1',
+                "id": 1,
                 "name": "Контора",
-                "isAdmin": "1",
+                "isAdmin": false,
                 "quizVms": [
                     {
-                        "id": "1",
+                        "id": 1,
+                        "finished": false,
                         "name": "Пятиминутка №15",
                         "description": "Пятиминутка о вёрстке и стилях",
                         "score": 0,
-                        "start-time" : "2019-07-26T12:43:23",
-                        "end-time" : "2022-07-26T12:43:23",
+                        "startTime" : "2019-07-26T12:43:23+05:00",
+                        "endTime" : "2025-07-26T12:43:23+05:00",
                     },
                     {
-                        "id": "2",
+                        "id": 2,
+                        "finished": false,
                         "name": "Пятиминутка №228",
                         "description": "Пятиминутка о вёрстке и стилях",
                         "score": 0,
-                        "start-time" : "2019-07-26T12:43:23",
-                        "end-time" : "2022-07-26T12:43:23",
+                        "startTime" : "2019-07-26T12:43:23+05:00",
+                        "endTime" : "2025-07-26T12:43:23+00:00",
                     }
                 ]
+            },
+            "groupVm2": {
+                "id": 1,
+                "name": "Контора2",
+                "isAdmin": true,
+                "quizVms": [
+                    {
+                        "id": 1,
+                        "finished": false,
+                        "name": "Пятиминутка №15",
+                        "description": "Пятиминутка о вёрстке и стилях",
+                        "score": 0,
+                        "startTime" : "2019-07-26T12:43:23+05:00",
+                        "endTime" : "2025-07-26T12:43:23+05:00",
+                    },
+                ]
+            },
+            "groupVm3": {
+                "id": 1,
+                "name": "Контора3",
+                "isAdmin": false,
+                "quizVms": [
+                    {
+                        "id": 1,
+                        "finished": true,
+                        "name": "Пятиминутка №15",
+                        "description": "Пятиминутка о вёрстке и стилях",
+                        "score": 0,
+                        "startTime" : "2019-07-26T12:43:23+05:00",
+                        "endTime" : "2025-07-26T12:43:23+05:00",
+                    },
+                ]
+            },
+            "groupVm4": {
+                "id": 1,
+                "name": "Контора4",
+                "isAdmin": false,
+                "quizVms": [
+                    {
+                        "id": 1,
+                        "finished": false,
+                        "name": "Моя первая пятиминутка",
+                        "description": "Пятиминутка ни о чём",
+                        "score": 0,
+                        "startTime" : "2019-07-26T12:43:23+05:00",
+                        "endTime" : "2025-07-26T12:43:23+05:00",
+                    },
+                ]
             }
+
         }
         return data;
     }
--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,19 +1,151 @@
+const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+const dateTimeOptions = {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+    timeZone: userTimeZone
+};
+
+let data;
 
 document.addEventListener('DOMContentLoaded', async function(event) {
     try {
 
-        const response = await fetch('http://localhost:8080/form/mainPage', {
+        let response = await fetch('http://localhost:8080/form/mainPage', {
             method: 'POST'
         });
 
-        console.log("Finished");
-        console.log(await response.json());
-        console.log("Finished");
+        data = await response.json();
+        console.log(data);
+
+        buildPage();
 
     } catch (e) {
         console.log(e);
     }
 });
+
+function buildPage() {
+    let activePaste = document.querySelector('#active-paste-place');
+
+    for (let groupKey in data) {
+        let currentGroup = data[groupKey];
+        if (currentGroup['isAdmin'])
+            continue;
+        let group = new ActiveGroupBlockDiv(currentGroup.name);
+        group.hide();
+        for (let quizKey in currentGroup.quizVms) {
+            let activeQuiz = currentGroup.quizVms[quizKey];
+            if (activeQuiz.finished)
+                continue;
+            let startTime = new Date(activeQuiz.startTime);
+            let endTime = new Date(activeQuiz.endTime);
+
+            if (startTime > Date.now() || endTime < Date.now())
+                continue;
+
+            group.addActiveQuiz(activeQuiz.name, endTime.toLocaleString('ru-RU', dateTimeOptions));
+        }
+        if (group.activeCount > 0) {
+            activePaste.appendChild(group.element);
+            group.show();
+        } else {
+            group.remove();
+        }
+    }
+}
+
+
+
+class CustomDOMElement {
+    constructor(tag) {
+        this.element = document.createElement(tag);
+        this._displayStyle = 'block';
+    }
+
+    hide() {
+        this._displayStyle = this.element.style.display;
+        this.element.style.display = 'none';
+    }
+
+    show() {
+        this.element.style.display = this._displayStyle;
+    }
+
+    remove() {
+        this.element.remove();
+    }
+
+    withClass(className) {
+        this.element.classList.add(className);
+        return this;
+    }
+
+    withContent(content) {
+        this.element.textContent = content;
+        return this;
+    }
+
+    appendChild(child) {
+        if (child instanceof CustomDOMElement) {
+            this.element.appendChild(child.element);
+        } else {
+            this.element.appendChild(child);
+        }
+    }
+
+    removeChild(child) {
+        if (child instanceof CustomDOMElement) {
+            this.element.removeChild(child.element);
+        }
+        else {
+            this.element.removeChild(child);
+        }
+    }
+}
+
+class ActiveGroupBlockDiv extends CustomDOMElement {
+    constructor(headerText) {
+        super('div').withClass('block');
+        this.appendChild(new GroupHeaderDiv(headerText));
+        this.activeCount = 0;
+    }
+
+    addActiveQuiz(header, endTime) {
+        this.appendChild(new ActiveQuizBtn(header, endTime));
+        this.activeCount++;
+    }
+
+}
+
+class GroupHeaderDiv extends CustomDOMElement {
+    constructor(headerText) {
+        super('div').withClass('group-name');
+        let h2 = new CustomDOMElement('h2').withContent(headerText);
+        let hr = new CustomDOMElement('hr');
+        this.appendChild(h2);
+        this.appendChild(hr);
+    }
+}
+
+class ActiveQuizBtn extends CustomDOMElement {
+    constructor(header, endTime) {
+        super('button').withClass('quiz-info').withClass('active-quiz');
+        let name = new CustomDOMElement('label').withClass('quiz-name').withContent(header);
+        let end = new CustomDOMElement('label')
+            .withClass('quiz-end-time')
+            .withContent(`Завершится: ${endTime}`);
+        this.appendChild(name);
+        this.appendChild(end);
+    }
+}
+
+
+
 
 class ButtonsGroupHandler {
     constructor(buttonsHandlers) {
