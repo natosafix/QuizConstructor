585289a004ba0337f61e589258d72329281b41ea
natosafix <92174064+natosafix@users.noreply.github.com>
2023-06-22 18:10:18 +0500

    Hotfix quizRouter

--- routes/quizRouter.js
+++ routes/quizRouter.js
@@ -4,7 +4,6 @@ const quizController = require('../conttoller/quizController');
 
 router.get('/edit/:id', quizController.editQuiz);
 router.get('/solve/:id', quizController.solveQuiz);
-router.get('/check/:id', quizController.checkQuiz)
-router.get('/groupSettings/:id', quizController.groupSettings)
+router.get('/check/:id', quizController.checkQuiz);
 
 module.exports = router;
