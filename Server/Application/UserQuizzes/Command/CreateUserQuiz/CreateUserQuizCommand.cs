c2f247a316cb38e587a23c5abaf7d5d949ab51e3
xyoouy <egorloparev@gmail.com>
2023-06-24 16:16:55 +0500

    Fix

---
+++ Server/Application/UserQuizzes/Command/CreateUserQuiz/CreateUserQuizCommand.cs
@@ -0,0 +1,14 @@
+﻿using Application.Inputs;
+using MediatR;
+
+namespace Application.UserQuizzes.Command.CreateUserQuiz;
+
+public class CreateUserQuizCommand : IRequest<int>
+{
+    public int QuizGroupId { get; set; }
+    public string UserLogin { get; set; }
+    
+    public DateTime Finished { get; set; }
+    
+    public List<UserQuestionInput> Questions { get; set; }
+}
