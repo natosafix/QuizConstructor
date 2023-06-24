58a7f36d0cdb57e28abf3864875a1c24eca25962
xyoouy <egorloparev@gmail.com>
2023-06-24 16:48:01 +0500

    + Add UserQuiz

---
+++ Server/Application/UserQuizzes/Command/CreateUserQuiz/CreateUserQuizCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using Application.Common.Abstracts;
+using Application.Interfaces;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UserQuizzes.Command.CreateUserQuiz;
+
+public class CreateUserQuizCommandHandler : RequestHandler, IRequestHandler<CreateUserQuizCommand, int>
+{
+    public CreateUserQuizCommandHandler(IDbContext context) : base(context) { }
+
+    public async Task<int> Handle(CreateUserQuizCommand request, CancellationToken cancellationToken)
+    {
+        var quizGroup = await context.QuizGroups
+            .FirstOrDefaultAsync(quizGroup => quizGroup.Id == request.QuizGroupId, cancellationToken);
+
+        var user = await context.Users
+            .FirstOrDefaultAsync(user => user.Login == request.UserLogin, cancellationToken);
+
+        var k = request.Questions.SelectMany(x => x.Answers)
+            .Select(x => new UserAnswer
+            {
+                Content = x.Content,
+                QuestionId = 
+            });
+        
+        var userQuiz = new UserQuiz
+        {
+            User = user,
+            EndTime = request.Finished,
+            QuizGroup = quizGroup,
+        }
+    }
+}
