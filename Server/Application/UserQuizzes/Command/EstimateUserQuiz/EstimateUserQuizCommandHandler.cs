c219066f4cbf7a923a0da50ba025fd72dab4f43f
xyoouy <egorloparev@gmail.com>
2023-06-26 01:25:49 +0500

    Add EstimateUserQuizCommand.cs

---
+++ Server/Application/UserQuizzes/Command/EstimateUserQuiz/EstimateUserQuizCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using Application.Common.Abstracts;
+using Application.Common.Exceptions;
+using Application.Interfaces;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UserQuizzes.Command.EstimateUserQuiz;
+
+public class EstimateUserQuizCommandHandler : RequestHandler, IRequestHandler<EstimateUserQuizCommand, int>
+{
+    public EstimateUserQuizCommandHandler(IDbContext context) : base(context) { }
+
+    public async Task<int> Handle(EstimateUserQuizCommand request, CancellationToken cancellationToken)
+    {
+        var userQuiz = await context.UserQuizzes
+            .Include(uq => uq.Questions)
+            .FirstOrDefaultAsync(uq => uq.Id == request.Id, cancellationToken);
+
+        if (userQuiz == null || userQuiz.Questions == null)
+            throw new NotFoundException(nameof(UserQuiz), request.Id);
+
+        var score = 0;
+        foreach (var point in request.Points)
+        {
+            userQuiz.Questions.FirstOrDefault(q => q.Id == point.QuestionId).Score = point.Score;
+            score += point.Score;
+        }
+
+        userQuiz.Score = score;
+        await context.SaveChangesAsync(cancellationToken);
+        return userQuiz.Id;
+    }
+}
