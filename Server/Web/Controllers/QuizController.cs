6e3d8fc866409c5d5502f7346af4019e8ea1027b
xyoouy <egorloparev@gmail.com>
2023-06-26 02:11:28 +0500

    fix autocheck

--- Server/Web/Controllers/QuizController.cs
+++ Server/Web/Controllers/QuizController.cs
@@ -1,4 +1,5 @@
-﻿using Application.Quizzes.Commands.CreateQuiz;
+﻿using Application.Common.Exceptions;
+using Application.Quizzes.Commands.CreateQuiz;
 using Application.Quizzes.Commands.UpdateQuiz;
 using Application.Quizzes.Queries.GetQuiz;
 using Application.Quizzes.Queries.GetQuizForUser;
@@ -6,50 +7,93 @@ using Application.Quizzes.Queries.GetQuizzesByUserLogin;
 using Application.UserQuizzes.Command.CreateUserQuiz;
 using Application.UserQuizzes.Command.EstimateUserQuiz;
 using Application.UserQuizzes.Query.GetUserQuiz;
-using AutoMapper;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 namespace Web.Controllers;
 
 [Route("api/[controller]")]
 public class QuizController : BaseController
 {
-    private readonly IMapper mapper;
-
-    public QuizController(IMapper mapper) => this.mapper = mapper;
-
     [HttpGet("getQuiz")]
     public async Task<ActionResult<int>> GetQuiz([FromQuery] GetQuizQuery command)
     {
-        var quizVm = await Mediator.Send(command);
-        return Ok(quizVm);
+        try
+        {
+            var quizVm = await Mediator.Send(command);
+            return Ok(quizVm);
+        }
+        catch (Exception ex)
+        {
+            if (ex is NotFoundException)
+                return NotFound();
+            return Forbid();
+        }
+        
     }
     
     [HttpGet("getQuizzesByLogin")]
     public async Task<ActionResult<int>> GetQuizzesByLogin([FromQuery] GetQuizzesByUserLoginQuery command)
     {
-        var quizListVm = await Mediator.Send(command);
-        return Ok(quizListVm);
+        try
+        {
+            var quizListVm = await Mediator.Send(command);
+            return Ok(quizListVm);
+        }
+        catch (Exception ex)
+        {
+            if (ex is NotFoundException)
+                return NotFound();
+            return Forbid();
+        }
     }
 
     [HttpPost("createQuiz")]
     public async Task<ActionResult<int>> CreateQuiz([FromBody] CreateQuizCommand command)
     {
-        var quizId = await Mediator.Send(command);
-        return Ok(quizId);
+        try
+        {
+            var quizId = await Mediator.Send(command);
+            return Ok(quizId);
+        }
+        catch (Exception ex)
+        {
+            if (ex is NotFoundException)
+                return NotFound();
+            return Forbid();
+        }
+        
     }
     
     [HttpPut("updateQuiz")]
     public async Task<ActionResult<int>> UpdateQuiz([FromBody] UpdateQuizCommand command)
     {
-        var quizId = await Mediator.Send(command);
-        return Ok(quizId);
+        try
+        {
+            var quizId = await Mediator.Send(command);
+            return Ok(quizId);
+        }
+        catch (Exception ex)
+        {
+            if (ex is NotFoundException)
+                return NotFound();
+            return Forbid();
+        }
     }
     
     [HttpGet("getQuizForUser")]
     public async Task<ActionResult<int>> GetQuizForUser([FromQuery] GetQuizForUserQuery command)
     {
-        var quiz = await Mediator.Send(command);
-        return Ok(quiz);
+        try
+        {
+            var quiz = await Mediator.Send(command);
+            return Ok(quiz);
+        }
+        catch (Exception ex)
+        {
+            if (ex is NotFoundException)
+                return NotFound();
+            return Forbid();
+        }
     }
     
     [HttpGet("getUserQuiz")]
