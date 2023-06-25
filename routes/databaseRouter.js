8b9f1c2aca66385ad64113ea9e6df7e323398151
natosafix <92174064+natosafix@users.noreply.github.com>
2023-06-25 16:14:51 +0500

    next step com

--- routes/databaseRouter.js
+++ routes/databaseRouter.js
@@ -4,5 +4,7 @@ const databaseController = require('../controllers/databaseController');
 
 router.get('/apiRequest', databaseController.sendGetRequest);
 router.post('/apiRequest', databaseController.sendPostRequest);
+router.put('/apiRequest', databaseController.sendPutRequest);
+router.delete('/apiRequest', databaseController.sendDeleteRequest);
 
 module.exports = router;
